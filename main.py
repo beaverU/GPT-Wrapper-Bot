95a91e93062932fb93062b27ed8ab08c625021bc
beaverU <ivanklapyshev@gmail.com>
2024-03-16 20:52:12 +0400

    some changes

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import logging
+import os
+import openai
 
 from telegram import ForceReply, Update
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+
+GPT_model = 'gpt-3.5-turbo'
 
 # Enable logging
 logging.basicConfig(
@@ -33,18 +37,25 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Echo the user message."""
     await update.message.reply_text(update.message.text)
 
+async def send_to_openai(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Message forwarding logic."""
+    client = openai.OpenAI()
+    response = client.chat.completions.create(
+        model = GPT_model,
+        messages=[{"role": "user", "content": update.message.text}])
+    await update.message.reply_text(response.choices[0].message.content)
 
 def main() -> None:
     """Start the bot."""
     # Create the Application and pass it your bot's token.
-    application = Application.builder().token("").build()
+    application = Application.builder().token(os.environ['TELEGRAM_BOT_TOKEN']).build()
 
     # on different commands - answer in Telegram
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
 
     # on non command i.e message - echo the message on Telegram
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, send_to_openai))
 
     # Run the bot until the user presses Ctrl-C
     application.run_polling(allowed_updates=Update.ALL_TYPES)
